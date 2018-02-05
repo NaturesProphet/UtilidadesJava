743b27c52c1d0b066585dc41cff3a5927268fe29
mgarcia <mateus.gigainfo@gmail.com>
2018-02-05 13:46:53 -0200

    Verificações se o arquivo existe nos metodos

--- Utilidades/src/GERAL/Arquivo.java
+++ Utilidades/src/GERAL/Arquivo.java
@@ -31,78 +31,100 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Arquivo {
 
     //lê a primeira linha de um arquivo informado pelo parâmetro
     public static String getFirstLine(String path) throws IOException {
-        BufferedReader buffRead = new BufferedReader(new FileReader(path));
-        String linha = buffRead.readLine();
-        buffRead.close();
-        return linha;
+        if (AreYouHere(path)) {
+            BufferedReader buffRead = new BufferedReader(new FileReader(path));
+            String linha = buffRead.readLine();
+            buffRead.close();
+            return linha;
+        } else {
+            throw new FileNotFoundException("O programa buscou por um arquivo "
+                    + "que não foi encontrado no local especificado");
+        }
     }
 
     //escreve o conteúdo da String no arquivo, informados via parâmetro
     public static void setFileContent(String path, String content)
             throws IOException {
-        BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
-        buffWrite.append(content + "\n");
-        buffWrite.close();
+        if (AreYouHere(path)) {
+            BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+            buffWrite.append(content + "\n");
+            buffWrite.close();
+        } else {
+            throw new FileNotFoundException("O programa buscou por um arquivo "
+                    + "que não foi encontrado no local especificado");
+        }
     }
 
     //este método lê as linhas de um arquivo e retorna uma lista de Strings
     //recebe o caminho para o arquivo como parâmetro
     public static ArrayList<String> getAllLines(String patch)
             throws FileNotFoundException, IOException {
-        ArrayList<String> dados = new ArrayList();
-        BufferedReader br = new BufferedReader(new FileReader(patch));
-        String linha;
-        Scanner scan = new Scanner(br);
-        while (scan.hasNextLine()) {
-            if (null != scan.nextLine()) {
-                linha = scan.nextLine();
-                dados.add(linha);
+        if (AreYouHere(patch)) {
+            ArrayList<String> dados = new ArrayList();
+            BufferedReader br = new BufferedReader(new FileReader(patch));
+            String linha;
+            Scanner scan = new Scanner(br);
+            while (scan.hasNextLine()) {
+                if (null != scan.nextLine()) {
+                    linha = scan.nextLine();
+                    dados.add(linha);
+                }
             }
+            br.close();
+            scan.close();
+            return dados;
+        } else {
+            throw new FileNotFoundException("O programa buscou por um arquivo "
+                    + "que não foi encontrado no local especificado");
         }
-        br.close();
-        scan.close();
-        return dados;
     }
 
     //retorna uma linha no arquivo que comece com a String informada.
     //a string retornada começa depois do final + 1 espaço do codigo
-    public static String getLineByCode(String patch, String codigo)
+    public static String getLineByCode(String path, String codigo)
             throws FileNotFoundException, IOException, Exception {
-        BufferedReader br = new BufferedReader(new FileReader(patch));
-        String linha; //linha atual de cada iteração
-        int contador = 0; //total de ocorrências do codigo
-        String saida = ""; //string que será retornada
-        Scanner scan = new Scanner(br);
+        if (AreYouHere(path)) {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String linha; //linha atual de cada iteração
+            int contador = 0; //total de ocorrências do codigo
+            String saida = ""; //string que será retornada
+            Scanner scan = new Scanner(br);
 
-        while (scan.hasNextLine()) {
-            linha = scan.nextLine();
-            if (linha.indexOf(codigo) != -1) {
-                if (contador > 0) { // se houver mais de uma ocorrência
-                    br.close();
-                    throw new Exception("Util.Arquivo: O Programa tentou "
-                            + "buscar no arquivo por uma referência que se "
-                            + "repete mais de uma vez e por isso ele não sabe "
-                            + "qual delas usar. Contate o Desenvolvedor");
-                } else {
-                    saida = linha;
-                    contador++;
+            while (scan.hasNextLine()) {
+                linha = scan.nextLine();
+                if (linha.indexOf(codigo) != -1) {
+                    if (contador > 0) { // se houver mais de uma ocorrência
+                        br.close();
+                        throw new Exception("Util.Arquivo: O Programa tentou "
+                                + "buscar no arquivo por uma referência que se "
+                                + "repete mais de uma vez e por isso ele não sabe "
+                                + "qual delas usar. Contate o Desenvolvedor");
+                    } else {
+                        saida = linha;
+                        contador++;
+                    }
                 }
             }
-        }
-        br.close();
-        scan.close();
-        if (contador == 0) {
-            return null;
+            br.close();
+            scan.close();
+            if (contador == 0) {
+                return null;
+            } else {
+                saida = saida.substring(codigo.length() + 1, saida.length());
+                //corta a substring desde o final do codigo mais 1 ate o final.
+                // o +1 serve para pular o espaço vazio após o codigo
+                return saida;
+            }
         } else {
-            saida = saida.substring(codigo.length() + 1, saida.length());
-            //corta a substring desde o final do codigo mais 1 ate o final.
-            // o +1 serve para pular o espaço vazio após o codigo
-            return saida;
+            throw new FileNotFoundException("O programa buscou por um arquivo "
+                    + "que não foi encontrado no local especificado");
         }
     }
 
@@ -127,16 +149,22 @@ public class Arquivo {
 
     //adiciona uma linha especificada no final do arquivo especificado
     public static void AddThisLineAtEOF(String path, String texto) throws IOException {
-        FileWriter fw = new FileWriter(path, true);
-        BufferedWriter bw = new BufferedWriter(fw);
-        bw.write(texto);
-        bw.newLine();
-        bw.close();
+        if (AreYouHere(path)) {
+            FileWriter fw = new FileWriter(path, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(texto);
+            bw.newLine();
+            bw.close();
+        } else {
+            throw new FileNotFoundException("O programa buscou por um arquivo "
+                    + "que não foi encontrado no local especificado");
+        }
 
     }
 
     public static void main(String[] args) {
         //testes aqui
+
     }
 
 }
